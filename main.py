1421f6b9c512eb5887111821eb01e78d47242695
andrewAlexander28 <chimboandrew28@gmail.com>
2024-06-18 10:02:31 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import math
-def calcular_raiz_cuadrada(numero):
-    if numero >= 0:
-        return math.sqrt(numero)
+def calcular_raiz_cuadrada(number):
+    if number >= 0:
+        return math.sqrt(number)
     else:
         return "There is no root a negative number"
 
-# Pedir al usuario que ingrese un número
-numero = float(input("Type a number to calculate its square root: "))
+number = float(input("Type a number to calculate its square root: "))
 
-# Calcular y mostrar el resultado
-resultado = calcular_raiz_cuadrada(numero)
-print(resultado)
+result = calcular_raiz_cuadrada(number)
+print(result)
